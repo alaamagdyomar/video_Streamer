b036b476ba33c957f8b84e3b0ee96c782419d803
alaa magdy <alaa_magdy@outlook.com>
2021-05-29 18:46:19 +0200

    edit&delete buttons design

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -8,10 +8,23 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderButtons (stream){
+    if ( stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+            <button className="ui button primary">Edit</button>
+            <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+}
+
+
   renderList (){
     return this.props.streams.map(stream=>{
       return (
         <div className="item" key={stream.id}>
+          {this.renderButtons(stream)}
           <i className="large middle aligned icon camera"/>
           <div className="content">
             {stream.title}
@@ -21,6 +34,8 @@ class StreamList extends React.Component {
       );
     })
   }
+
+  
 
   render () {
     return (
@@ -33,7 +48,10 @@ class StreamList extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-   return { streams : Object.values(state.streams)}
+   return { 
+     streams : Object.values(state.streams),
+     currentUserId: state.auth.userId
+    }
 };
 
 export default connect(mapStateToProps , { fetchStreams })(StreamList);
