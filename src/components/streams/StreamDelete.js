c2fa138611639e9518e593e12333a3b0610de243
alaa magdy <alaa_magdy@outlook.com>
2021-06-01 22:24:40 +0200

    portal is created and disigned

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Modal from '../Modal';
 
 const StreamDelete = () => {
-  return <div>StreamDelete</div>;
+  return (
+  <div>
+       Stream Delete
+      <Modal />
+  </div>
+  )
 };
 
 export default StreamDelete;
